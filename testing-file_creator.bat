4571cb077409851d755314dc46ef2600dc5bf4c5
AfzGit <sa15@duck.com>
2025-05-17 23:20:41 +0300

    updated test script for windows

--- testing-file_creator.bat
+++ testing-file_creator.bat
@@ -1,3 +1,5 @@
+mkdir test\folder
+cd test
 type nul > file1.txt
 type nul > file2.md
 type nul > file3.py
@@ -6,3 +8,7 @@ type nul > file5.TXT
 type nul > file6
 type nul > file7.jpeg
 type nul > file8.PY
+cd folder
+type nul > file.txt
+type nul > file4.py
+type nul > file2.mp4
